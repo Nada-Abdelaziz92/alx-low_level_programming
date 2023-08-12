4d745c0dea23aaf433cf147dbadff3e1b24bb956
nada <eng.nada@icloud.com>
2023-08-11 17:14:37 -0700

    task 1

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+    int d;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	 d = n % 10;
+	if (d == 0)  printf("Last digit of %d is %d and is 0\n", n,d );
+	else if ( d > 5 ) printf("Last digit of %d is %d and is greater than 5\n", n,d );
+	else printf("Last digit of %d is %d and is less than 6 and not 0\n", n,d );
+	return (0);
+}
