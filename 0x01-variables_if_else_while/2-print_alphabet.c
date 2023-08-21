68a25ae79161697a956c237dc02dae9386f29316
nada <eng.nada@icloud.com>
2023-08-21 11:56:50 -0700

    test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        int alphabet = 'a';
+	int alphabet = 'a';
 
-        for ( ; alphabet <= 'z' ; alphabet++)
-        {
-        putchar(alphabet);
-        }
-        putchar('\n');
-        return (0);
+	for ( ; alphabet <= 'z' ; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 
 }
